99c416ce614353d7500066bffacef737262231e1
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-12-05 23:37:07 -0800

    Replaced storyline + prompts w/ links

--- aoc_2021/src/aoc/day00.rs
+++ aoc_2021/src/aoc/day00.rs
@@ -4,24 +4,17 @@ pub struct Day00 {
     num: usize
 }
 
+// Testing
 impl AoCProblem<usize, usize> for Day00 {
     fn prepare(input: Vec<String>) -> Self {
         let num = input[0].parse().unwrap();
         return Day00 { num };
     }
 
-    // --- Day 0: Test Round ---
-    //
-    // This is a test round to make sure everything works as intended.
-    //
-    // Suppose you are given a number x (your puzzle input). What is x + 10?
     fn part1(&self) -> usize {
         return self.num + my_helper_function();
     }
 
-    // --- Part Two ---
-    //
-    // Suppose I wanted to find x + 1000 + 10 * 10. What is it?
     fn part2(&self) -> usize {
         return self.num + 1000 + my_helper_function() * 10;
     }
