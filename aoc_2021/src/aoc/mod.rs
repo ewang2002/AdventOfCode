b3d6f4904011b77cb4cdbf938246e0296a623d91
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-12-10 13:48:55 -0800

    2021 Use a macro to automatically parse input and prepare solution

--- aoc_2021/src/aoc/mod.rs
+++ aoc_2021/src/aoc/mod.rs
@@ -1,12 +1,36 @@
+#![allow(dead_code)]
+
 pub mod aoc_problem;
+
 pub mod day00;
+pub use day00::Day00;
+
 pub mod day01;
+pub use day01::Day01;
+
 pub mod day02;
+pub use day02::Day02;
+
 pub mod day03;
+pub use day03::Day03;
+
 pub mod day04;
+pub use day04::Day04;
+
 pub mod day05;
+pub use day05::Day05;
+
 pub mod day06;
+pub use day06::Day06;
+
 pub mod day07;
+pub use day07::Day07;
+
 pub mod day08;
+pub use day08::Day08;
+
 pub mod day09;
+pub use day09::Day09;
+
 pub mod day10;
+pub use day10::Day10;
--- aoc_2021/src/main.rs
+++ aoc_2021/src/main.rs
@@ -1,19 +1,37 @@
+#![allow(unused_mut, unused_imports)]
+
 use std::time::Instant;
 use helpers::io;
-
-#[allow(unused_imports)]
-use aoc::aoc_problem::AoCProblem;
-use crate::aoc::day10::Day10;
+use crate::aoc::aoc_problem::AoCProblem;
 
 mod aoc;
 mod helpers;
 
-fn main() {
-    let input_file = io::file_read_all_lines("input/day10.txt");
+/// A macro that can be used to automatically parse the input file and prepare the corresponding
+/// problem structure for execution. To use:
+/// ```
+/// let mut solver = prepare_day!(DayXX);
+/// ```
+/// Where `XX` is the day that you want to execute. For example, to execute:
+/// - The solutions for Day 3, use `03`: `prepare_day!(Day03);`
+/// - The solutions for Day 10, use `10`: `prepare_day!(Day10);`
+macro_rules! prepare_day {
+    ($day: ident) => {{
+        #[allow(unused_imports)]
+        use crate::aoc::*;
+        let input_file = io::file_read_all_lines(
+            format!("input/{}.txt", stringify!($day).to_lowercase()).as_str()
+        );
+        $day::prepare(input_file)
+    }};
+}
 
+fn main() {
     // Prepare to solve
     let mut start = Instant::now();
-    let mut solver = Day10::prepare(input_file);
+
+    // Change this to the correct day!
+    let mut solver = prepare_day!(Day10);
     let in_t = start.elapsed();
 
     // Execution begins
