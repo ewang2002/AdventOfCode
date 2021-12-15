1dff30c49f7c63702475f96b1f0cb53a5a825b46
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-12-14 17:23:08 -0800

    2021 day 14 day 2

--- aoc_2021/src/aoc/day14.rs
+++ aoc_2021/src/aoc/day14.rs
@@ -1,49 +1,96 @@
-use std::collections::HashMap;
+use std::collections::{HashMap};
 use crate::aoc::aoc_problem::AoCProblem;
 
 pub struct Day14 {
     polymer_template: String,
-    polymer_rules: HashMap<String, char>
+    polymer_rules: HashMap<[char; 2], char>,
 }
 
 impl AoCProblem<usize, usize> for Day14 {
     fn prepare(input: Vec<String>) -> Self {
-        let mut polymer_rules: HashMap<String, char> = HashMap::new();
+        let mut polymer_rules: HashMap<[char; 2], char> = HashMap::new();
         for (rule, res) in input.iter().skip(2).map(|x| x.split_once(" -> ").unwrap()) {
-            polymer_rules.insert(rule.to_string(), res.chars().nth(0).unwrap());
+            let c = rule.chars().collect::<Vec<_>>();
+            polymer_rules.insert([c[0], c[1]], res.chars().nth(0).unwrap());
         }
 
         Self {
             polymer_template: input[0].to_string(),
-            polymer_rules
+            polymer_rules,
         }
     }
 
+
     fn part1(&self) -> usize {
-        let mut polymer = self.polymer_template.chars().collect::<Vec<_>>();
+        let mut frequency: HashMap<[char; 2], usize> = HashMap::new();
+        let polymer = self.polymer_template.chars().collect::<Vec<_>>();
+        for w in polymer.windows(2) {
+            *frequency.entry([w[0], w[1]]).or_insert(0) += 1;
+        }
+
+        let mut count: HashMap<char, usize> = HashMap::new();
         for _ in 0..10 {
-            let mut new_polymer: Vec<char> = vec![];
-            for c in polymer.windows(2) {
-                new_polymer.push(c[0]);
-                new_polymer.push(*self.polymer_rules.get(&format!("{}{}", c[0], c[1])).unwrap());
-            }
-
-            new_polymer.push(polymer[polymer.len() - 1]);
-            polymer = new_polymer;
+            let (freq, ct) = get_frequency_of_pairs(frequency, &self.polymer_rules);
+            frequency = freq;
+            count = ct;
         }
 
-        let mut counts: HashMap<char, usize> = HashMap::new();
-        for c in polymer {
-            *counts.entry(c).or_insert(0) += 1;
-        }
+        *count.get_mut(polymer.iter().last().unwrap()).unwrap() += 1;
 
-        let (_, max_ct) = counts.iter().max_by_key(|&(_, count)| count).unwrap();
-        let (_, min_ct) = counts.iter().min_by_key(|&(_, count)| count).unwrap();
+        let (_, max_ct) = count.iter().max_by_key(|&(_, count)| count).unwrap();
+        let (_, min_ct) = count.iter().min_by_key(|&(_, count)| count).unwrap();
 
         max_ct - min_ct
     }
 
+
     fn part2(&self) -> usize {
-        0
+        let mut frequency: HashMap<[char; 2], usize> = HashMap::new();
+        let polymer = self.polymer_template.chars().collect::<Vec<_>>();
+        for w in polymer.windows(2) {
+            *frequency.entry([w[0], w[1]]).or_insert(0) += 1;
+        }
+
+        let mut count: HashMap<char, usize> = HashMap::new();
+        for _ in 0..40 {
+            let (freq, ct) = get_frequency_of_pairs(frequency, &self.polymer_rules);
+            frequency = freq;
+            count = ct;
+        }
+
+        *count.get_mut(polymer.iter().last().unwrap()).unwrap() += 1;
+
+        let (_, max_ct) = count.iter().max_by_key(|&(_, count)| count).unwrap();
+        let (_, min_ct) = count.iter().min_by_key(|&(_, count)| count).unwrap();
+
+        max_ct - min_ct
     }
+}
+
+/// Gets the frequency of each pair that appears in the initial map.
+///
+/// # Parameters
+/// - `initial`: The initial polymer, decomposed as pairs.
+/// - `mapping`: The polymer mapping.
+///
+/// # Returns
+/// A tuple containing a hashmap representing the new pairings, and another hashmap consisting of
+/// the count of each character (excluding the last character that appeared in the original
+/// polymer).
+fn get_frequency_of_pairs(
+    initial: HashMap<[char; 2], usize>,
+    mapping: &HashMap<[char; 2], char>,
+) -> (HashMap<[char; 2], usize>, HashMap<char, usize>) {
+    let mut new_freq: HashMap<[char; 2], usize> = HashMap::new();
+    let mut count: HashMap<char, usize> = HashMap::new();
+
+    for (pair, ct) in initial {
+        let mapped_char = mapping.get(&pair).unwrap();
+        *new_freq.entry([pair[0], *mapped_char]).or_insert(0) += ct;
+        *new_freq.entry([*mapped_char, pair[1]]).or_insert(0) += ct;
+        *count.entry(pair[0]).or_insert(0) += ct;
+        *count.entry(*mapped_char).or_insert(0) += ct;
+    }
+
+    (new_freq, count)
 }
