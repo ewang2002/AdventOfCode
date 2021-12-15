3d5c683d5e027051507dc6c93f111c6cd8364392
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-12-14 17:31:39 -0800

    2021 day 14 day 2

--- aoc_2021/src/aoc/day14.rs
+++ aoc_2021/src/aoc/day14.rs
@@ -1,70 +1,70 @@
 use std::collections::{HashMap};
 use crate::aoc::aoc_problem::AoCProblem;
 
+type Pair = [char; 2];
+
 pub struct Day14 {
-    polymer_template: String,
-    polymer_rules: HashMap<[char; 2], char>,
+    polymer_template_pairing: HashMap<Pair, usize>,
+    polymer_rules: HashMap<Pair, char>,
+    last_polymer_char: char
 }
 
 impl AoCProblem<usize, usize> for Day14 {
     fn prepare(input: Vec<String>) -> Self {
-        let mut polymer_rules: HashMap<[char; 2], char> = HashMap::new();
+        let mut polymer_rules: HashMap<Pair, char> = HashMap::new();
         for (rule, res) in input.iter().skip(2).map(|x| x.split_once(" -> ").unwrap()) {
             let c = rule.chars().collect::<Vec<_>>();
             polymer_rules.insert([c[0], c[1]], res.chars().nth(0).unwrap());
         }
 
+        let mut polymer_template_pairing: HashMap<Pair, usize> = HashMap::new();
+        let polymer = input[0].chars().collect::<Vec<_>>();
+        for w in polymer.windows(2) {
+            *polymer_template_pairing.entry([w[0], w[1]]).or_insert(0) += 1;
+        }
+
         Self {
-            polymer_template: input[0].to_string(),
+            polymer_template_pairing,
             polymer_rules,
+            last_polymer_char: *polymer.iter().last().unwrap()
         }
     }
 
 
     fn part1(&self) -> usize {
-        let mut frequency: HashMap<[char; 2], usize> = HashMap::new();
-        let polymer = self.polymer_template.chars().collect::<Vec<_>>();
-        for w in polymer.windows(2) {
-            *frequency.entry([w[0], w[1]]).or_insert(0) += 1;
-        }
-
-        let mut count: HashMap<char, usize> = HashMap::new();
-        for _ in 0..10 {
-            let (freq, ct) = get_frequency_of_pairs(frequency, &self.polymer_rules);
-            frequency = freq;
-            count = ct;
-        }
-
-        *count.get_mut(polymer.iter().last().unwrap()).unwrap() += 1;
-
-        let (_, max_ct) = count.iter().max_by_key(|&(_, count)| count).unwrap();
-        let (_, min_ct) = count.iter().min_by_key(|&(_, count)| count).unwrap();
-
-        max_ct - min_ct
+        get_max_min_quantity_diff(self, 10)
     }
 
 
     fn part2(&self) -> usize {
-        let mut frequency: HashMap<[char; 2], usize> = HashMap::new();
-        let polymer = self.polymer_template.chars().collect::<Vec<_>>();
-        for w in polymer.windows(2) {
-            *frequency.entry([w[0], w[1]]).or_insert(0) += 1;
-        }
-
-        let mut count: HashMap<char, usize> = HashMap::new();
-        for _ in 0..40 {
-            let (freq, ct) = get_frequency_of_pairs(frequency, &self.polymer_rules);
-            frequency = freq;
-            count = ct;
-        }
-
-        *count.get_mut(polymer.iter().last().unwrap()).unwrap() += 1;
-
-        let (_, max_ct) = count.iter().max_by_key(|&(_, count)| count).unwrap();
-        let (_, min_ct) = count.iter().min_by_key(|&(_, count)| count).unwrap();
-
-        max_ct - min_ct
+        get_max_min_quantity_diff(self, 40)
     }
+}
+
+/// Gets the difference between the most and least occurring polymers.
+///
+/// # Parameters
+/// - `p_struct`: The Day14 problem structure.
+/// - `amt`: The number of times to run the frequency function.
+///
+/// # Returns
+/// The difference between the most and least occurring polymers.
+#[inline(always)]
+fn get_max_min_quantity_diff(p_struct: &Day14, amt: usize) -> usize {
+    let mut frequency: HashMap<Pair, usize> = p_struct.polymer_template_pairing.clone();
+    let mut count: HashMap<char, usize> = HashMap::new();
+    for _ in 0..amt {
+        let (freq, ct) = get_frequency_of_pairs(frequency, &p_struct.polymer_rules);
+        frequency = freq;
+        count = ct;
+    }
+
+    *count.get_mut(&p_struct.last_polymer_char).unwrap() += 1;
+
+    let (_, max_ct) = count.iter().max_by_key(|&(_, count)| count).unwrap();
+    let (_, min_ct) = count.iter().min_by_key(|&(_, count)| count).unwrap();
+
+    max_ct - min_ct
 }
 
 /// Gets the frequency of each pair that appears in the initial map.
@@ -78,10 +78,10 @@ impl AoCProblem<usize, usize> for Day14 {
 /// the count of each character (excluding the last character that appeared in the original
 /// polymer).
 fn get_frequency_of_pairs(
-    initial: HashMap<[char; 2], usize>,
-    mapping: &HashMap<[char; 2], char>,
-) -> (HashMap<[char; 2], usize>, HashMap<char, usize>) {
-    let mut new_freq: HashMap<[char; 2], usize> = HashMap::new();
+    initial: HashMap<Pair, usize>,
+    mapping: &HashMap<Pair, char>,
+) -> (HashMap<Pair, usize>, HashMap<char, usize>) {
+    let mut new_freq: HashMap<Pair, usize> = HashMap::new();
     let mut count: HashMap<char, usize> = HashMap::new();
 
     for (pair, ct) in initial {
