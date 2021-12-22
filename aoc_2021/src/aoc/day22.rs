512ad464e4242d209ed0018e48028e7976f547f0
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-12-21 22:46:06 -0800

    2021 day 22 part 1

---
+++ aoc_2021/src/aoc/day22.rs
@@ -0,0 +1,79 @@
+use std::collections::{HashSet};
+use crate::aoc::aoc_problem::AoCProblem;
+
+pub struct Day22 {
+    reboot_steps: Vec<RebootStep>,
+}
+
+impl AoCProblem<usize, usize> for Day22 {
+    fn prepare(input: Vec<String>) -> Self {
+        let mut reboot_steps: Vec<RebootStep> = vec![];
+        for line in input {
+            // on x=10..12,y=10..12,z=10..12
+            let (toggle_val, rest) = line.split_once(" ").unwrap();
+            let parsed_rest = rest
+                .replace("x=", "")
+                .replace(",y=", " ")
+                .replace(",z=", " ")
+                .replace("..", " ");
+
+            let mut iterator = parsed_rest.split(" ")
+                .map(|x| x.parse::<i32>().unwrap())
+                .into_iter();
+
+            reboot_steps.push(RebootStep {
+                toggle: toggle_val == "on",
+                from_x: iterator.next().unwrap(),
+                to_x: iterator.next().unwrap(),
+                from_y: iterator.next().unwrap(),
+                to_y: iterator.next().unwrap(),
+                from_z: iterator.next().unwrap(),
+                to_z: iterator.next().unwrap(),
+            });
+        }
+
+        Self {
+            reboot_steps
+        }
+    }
+
+    fn part1(&self) -> usize {
+        let mut enabled: HashSet<(i32, i32, i32)> = HashSet::new();
+        for reboot_step in &self.reboot_steps {
+            if reboot_step.from_x < -50 || reboot_step.to_x > 50 || reboot_step.from_y < -50
+                || reboot_step.to_y > 50 || reboot_step.from_z < -50 || reboot_step.to_z > 50 {
+                continue;
+            }
+
+            for x in reboot_step.from_x..=reboot_step.to_x {
+                for y in reboot_step.from_y..=reboot_step.to_y {
+                    for z in reboot_step.from_z..=reboot_step.to_z {
+                        if reboot_step.toggle {
+                            enabled.insert((x, y, z));
+                            continue;
+                        }
+
+                        enabled.remove(&(x, y, z));
+                    }
+                }
+            }
+        }
+
+        enabled.len()
+    }
+
+    fn part2(&self) -> usize {
+        0
+    }
+}
+
+#[derive(Copy, Clone)]
+struct RebootStep {
+    from_x: i32,
+    to_x: i32,
+    from_y: i32,
+    to_y: i32,
+    from_z: i32,
+    to_z: i32,
+    toggle: bool,
+}
--- aoc_2021/src/aoc/mod.rs
+++ aoc_2021/src/aoc/mod.rs
@@ -65,3 +65,6 @@ pub use day20::Day20;
 
 mod day21;
 pub use day21::Day21;
+
+mod day22;
+pub use day22::Day22;
