ad0499afcad8b07d9bddf43a61cb40994a6768d8
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-05-12 22:30:17 -0700

    2019 day6 minor changes

--- aoc_2019/src/aoc/day06.rs
+++ aoc_2019/src/aoc/day06.rs
@@ -12,6 +12,7 @@ impl AoCProblem<usize, usize> for Day06 {
     }
 
     fn part1(&mut self) -> usize {
+        // We want the graph to be "one-way" (i.e. directed) so we don't double-count
         let mut graph: HashMap<String, Vec<String>> = HashMap::new();
         for map_entry in &self.input {
             let (to, from) = map_entry.split_once(')').unwrap();
@@ -25,6 +26,7 @@ impl AoCProblem<usize, usize> for Day06 {
     }
 
     fn part2(&mut self) -> usize {
+        // Want the graph to be "both-ways" (i.e. undirected) so we can find a path
         let mut graph: HashMap<String, Vec<String>> = HashMap::new();
         for map_entry in &self.input {
             let (to, from) = map_entry.split_once(')').unwrap();
@@ -58,6 +60,7 @@ impl AoCProblem<usize, usize> for Day06 {
         explored.insert(start, 0);
         let default = vec![];
 
+        // BFS time!
         while let Some(key) = queue.pop_front() {
             let num = *explored.get(key).unwrap();
 
@@ -79,6 +82,21 @@ impl AoCProblem<usize, usize> for Day06 {
     }
 }
 
+/// Calculates the number of possible paths from node A to node B, including subsets.
+///
+/// For example, if we have the graph
+/// ```
+/// A -- x -- y -- B
+/// ```
+/// then there are 6 paths possible (Ax, Ay, AB, xy, xB, yB).
+///
+/// # Parameters
+/// - `graph`: The graph.
+/// - `planet`: The planet to check.
+/// - `height`: The number of nodes checked in this "path."
+///
+/// # Returns
+/// The number of paths
 fn calculate_paths(graph: &HashMap<String, Vec<String>>, planet: &str, height: usize) -> usize {
     let neighbors = graph.get(planet);
     if let Some(neighbors) = neighbors {
