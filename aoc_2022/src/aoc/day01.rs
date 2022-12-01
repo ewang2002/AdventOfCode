77dbee6f366912cec7928cd7cada548b19571714
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2022-12-01 07:53:21 -0500

    2022: day 1 slightly optimized

--- aoc_2022/src/aoc/day01.rs
+++ aoc_2022/src/aoc/day01.rs
@@ -1,31 +1,25 @@
 use crate::aoc::aoc_problem::AoCProblem;
 
 pub struct Day01 {
-    calories_raw: Vec<String>,
+    grouped_calories: Vec<usize>,
 }
 
 impl AoCProblem<usize, usize> for Day01 {
     fn prepare(input: &str) -> Self {
-        Self {
-            calories_raw: input.split("\r\n\r\n").map(|x| x.to_owned()).collect(),
-        }
+        let mut grouped_calories: Vec<usize> = input
+            .split("\r\n\r\n")
+            .map(|g| g.lines().map(|l| l.parse::<usize>().unwrap()).sum())
+            .collect();
+
+        grouped_calories.sort_by(|a, b| b.cmp(a));
+        Self { grouped_calories }
     }
 
     fn part1(&mut self) -> usize {
-        self.calories_raw
-            .iter()
-            .map(|c| c.lines().map(|x| x.parse::<usize>().unwrap()).sum())
-            .max()
-            .unwrap()
+        *self.grouped_calories.iter().max().unwrap()
     }
 
     fn part2(&mut self) -> usize {
-        let mut d = self
-            .calories_raw
-            .iter()
-            .map(|c| c.lines().map(|x| x.parse::<usize>().unwrap()).sum())
-            .collect::<Vec<usize>>();
-        d.sort_by(|a, b| b.partial_cmp(a).unwrap());
-        d.into_iter().take(3).sum()
+        self.grouped_calories.iter().take(3).sum()
     }
 }
