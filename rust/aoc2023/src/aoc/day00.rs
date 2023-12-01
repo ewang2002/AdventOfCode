aec419a672b1a31739989aa9756a648017e495f9
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2023-11-30 21:07:44 -0500

    chore: add aoc2023 bin

---
+++ rust/aoc2023/src/aoc/day00.rs
@@ -0,0 +1,27 @@
+﻿use common::problem::day::{AoCProblem, Solution};
+
+pub struct Day00 {
+    // fields here
+}
+
+impl AoCProblem for Day00 {
+    fn prepare(_input: String) -> Self {
+        Self {}
+    }
+
+    fn part1(&mut self) -> Solution {
+        0.into()
+    }
+
+    fn part2(&mut self) -> Solution {
+        0.into()
+    }
+
+    fn day() -> u32 {
+        0
+    }
+
+    fn year() -> u32 {
+        2023
+    }
+}
---
+++ rust/aoc2023/src/aoc/mod.rs
@@ -0,0 +1,2 @@
+mod day00;
+pub use day00::Day00;
