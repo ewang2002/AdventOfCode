7068677f5edf346a226c9014697727d72d0dfe10
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2023-12-02 21:13:55 -0500

    2023 day 2 complete

---
+++ rust/aoc2023/src/aoc/day02.rs
@@ -0,0 +1,93 @@
+﻿use std::cmp::max;
+
+use common::problem::day::{AoCProblem, Solution};
+
+const BLUE_INDEX: usize = 0;
+const RED_INDEX: usize = 1;
+const GREEN_INDEX: usize = 2;
+
+pub struct Day02 {
+    // game_info - list of games
+    // game_info[i] - the game information for the (i + 1)th game
+    // game_info[i][j] - the jth subset containing information about what was revealed
+    game_info: Vec<Vec<[usize; 3]>>,
+}
+
+impl AoCProblem for Day02 {
+    fn prepare(input: String) -> Self {
+        let mut game_info = vec![];
+
+        // For each game (round)
+        for game in input.lines() {
+            let mut this_game = vec![];
+            // We only care about the subset of cubes
+            let (_, data) = game.split_once(": ").unwrap();
+
+            // For each subset of cubes
+            for subset in data.split("; ") {
+                let mut parsed_subset = [0; 3];
+
+                // For each set of cubes that were revealed
+                for cube_info in subset.split(", ") {
+                    let (amt, color) = cube_info.split_once(' ').unwrap();
+                    let cube_color_idx = match color {
+                        "blue" => BLUE_INDEX,
+                        "red" => RED_INDEX,
+                        "green" => GREEN_INDEX,
+                        _ => panic!("unknown color '{color}'"),
+                    };
+
+                    let num_cubes = amt.parse::<usize>().unwrap();
+                    parsed_subset[cube_color_idx] += num_cubes;
+                }
+
+                this_game.push(parsed_subset);
+            }
+
+            game_info.push(this_game);
+        }
+
+        Self { game_info }
+    }
+
+    fn part1(&mut self) -> Solution {
+        self.game_info
+            .iter()
+            .enumerate()
+            .filter(|(_, rounds)| {
+                rounds.iter().all(|round| {
+                    round[RED_INDEX] <= 12 && round[GREEN_INDEX] <= 13 && round[BLUE_INDEX] <= 14
+                })
+            })
+            .map(|(idx, _)| idx + 1)
+            .sum::<usize>()
+            .into()
+    }
+
+    fn part2(&mut self) -> Solution {
+        self.game_info
+            .iter()
+            .map(|rounds| {
+                let mut min_red = 0;
+                let mut min_green = 0;
+                let mut min_blue = 0;
+                for [blue, red, green] in rounds {
+                    min_red = max(min_red, *red);
+                    min_green = max(min_green, *green);
+                    min_blue = max(min_blue, *blue);
+                }
+
+                min_red * min_green * min_blue
+            })
+            .sum::<usize>()
+            .into()
+    }
+
+    fn day() -> u32 {
+        2
+    }
+
+    fn year() -> u32 {
+        2023
+    }
+}
--- rust/aoc2023/src/main.rs
+++ rust/aoc2023/src/main.rs
@@ -1,4 +1,4 @@
-use common::problem::run;
+﻿use common::problem::run;
 use std::env;
 mod aoc;
 
@@ -30,6 +30,7 @@ fn main() {
     match day_to_use {
         0 => run::<crate::aoc::Day00>(test_case),
         1 => run::<crate::aoc::Day01>(test_case),
+        2 => run::<crate::aoc::Day02>(test_case),
         _ => {
             eprintln!("[Error] Day {day_to_use} has not been implemented yet.");
         }
